5dd8900ce5ff08f1efc9ca90e124585b2de8a57c
Christina <kriscat@mail.ru>
2022-08-12 23:14:51 +0500

    upd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { ConfigProvider } from 'antd';
 import ru_RU from 'antd/es/locale/ru_RU';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <React.StrictMode>
-    <BrowserRouter>
+
+  <React.StrictMode>
+    <HashRouter>
       <ConfigProvider locale={ru_RU}>
-    <App />
-  </ConfigProvider>
-    </BrowserRouter>
-    </React.StrictMode>
-   
+        <App />
+      </ConfigProvider>
+    </HashRouter>
+  </React.StrictMode>
+
 );
